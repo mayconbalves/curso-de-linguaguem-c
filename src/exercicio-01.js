d9e9ff53a97f3c2d04cddbd26e287f139cc9423c
mayconbalves <mayconmustaine@hotmail.com>
2021-03-25 21:34:57 -0300

    exercicio para trocar variaveis A e B

---
+++ src/exercicio-01.js
@@ -0,0 +1,14 @@
+console.log('Exercicio 01')
+let a = 10
+let b = 20
+let temp = 0
+console.log(`Variavel a ${a}`)
+console.log(`Variavel b ${b}`)
+
+temp = a
+a = b
+b = temp
+
+console.log('============')
+console.log(`Variavel a ${a}`)
+console.log(`Variavel b ${b}`)
