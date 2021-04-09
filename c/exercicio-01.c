371570833ee6af298d08f4d694430596b3ae4a44
mayconbalves <mayconmustaine@hotmail.com>
2021-04-08 23:17:03 -0300

    exercicio de troca de valores em C

---
+++ c/exercicio-01.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <assert.h>
+
+int main (){
+
+    int a, b, temp;
+    a = 999;
+    b = 555;
+
+    //
+    // Sua lógica
+    //
+    temp = a;     // registramos o valor de 'a' em 'temp'
+    a    = b;     // sobrescrever 'a' com o valor de 'b'
+    b    = temp;  // sobrescrever 'b' com o valor que era de 'a' (e foi registrado em 'temp')
+
+    //
+    // Testes
+    //
+    assert(a == 555);
+    assert(b == 999);
+
+    return 0;
+}
